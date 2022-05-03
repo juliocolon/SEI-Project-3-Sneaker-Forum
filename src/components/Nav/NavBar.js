8cff543fa34b9f0e55a2de30eb6c4490533aecc3
Julio Colon <jcolon1092@gmail.com>
2022-05-03 19:08:35 -0400

    added form modals to sign up and log in to the auth page

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -2,25 +2,107 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 
 
 export default function NavBar() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [login, setLogIn] = useState(false);
+  const handleCloseLogIn = () => setLogIn(false);
+  const handleShowLogIn = () => setLogIn(true);
+
+
+
+
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="/">Sneaker Forum</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav>
-        <Nav.Link href="/signup">Sign Up</Nav.Link>
-        <Nav.Link eventKey={2} href="/login">
-          Log In
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-    </Container>
-  </Navbar>
+    <>
+
+
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Sneaker Forum</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link onClick={handleShow}> Sign Up </Nav.Link>
+              <Nav.Link eventKey={2} onClick={handleShowLogIn}>   Log In </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name="email" autoFocus required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1" >
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" name="firstname" required />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" name="lastname" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" required />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+
+
+
+
+
+      <Modal show={login} onHide={handleCloseLogIn}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log In</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name="email" autoFocus required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" name="password" required />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
