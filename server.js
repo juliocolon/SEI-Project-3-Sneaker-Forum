2a7ed0ceeb1d967d87b49f903a4ed19c58966f6f
Julio Colon <jcolon1092@gmail.com>
2022-06-11 08:47:14 -0400

    updating code for heroku

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${PORT}`)
