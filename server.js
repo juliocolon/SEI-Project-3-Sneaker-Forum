09e1dead5e18afb8860798695ca0cf3c4190d1b4
Julio Colon <jcolon1092@gmail.com>
2022-06-11 08:38:52 -0400

    change to server.js

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.get('/*', function(req, res) {
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+  console.log(`Express app running on port ${PORT}`)
 });
